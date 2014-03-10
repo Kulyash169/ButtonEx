0097fb47699993d9c83c22ee9bf9425afd19fbf1
Kulyash Orazbekova <apple@Kulyash.local>
2014-03-10 16:31:03 +0600

    Initial Commit

---
+++ ButtonEx/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ButtonEx
+//
+//  Created by Kulyash Orazbekova on 10.03.14.
+//  Copyright (c) 2014 Kulyash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
